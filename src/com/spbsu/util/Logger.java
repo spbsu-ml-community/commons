307d55e1893b8cc239f9e62412399a6d5779adf3
alms <alms@yandex-team.ru>
2009-07-21 19:12:30 +0000

    - logger: use apache common logging with log4j as back end

--- src/com/spbsu/util/Logger.java
+++ src/com/spbsu/util/Logger.java
@@ -1,67 +1,142 @@
 package com.spbsu.util;
 
-import java.util.logging.Level;
-import java.util.logging.LogManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @author solar
  * @author lyadzhin
  */
-public class Logger extends java.util.logging.Logger {
+public class Logger implements Log {
+
   public static Logger create(Class clazz) {
     return create(clazz.getName());
   }
   
   public static Logger create(String name) {
-    LogManager manager = LogManager.getLogManager();
-    Logger result = (Logger)manager.getLogger(name);
-    if (result == null) {
-        result = new Logger(name);
-        manager.addLogger(result);
-        result = (Logger)manager.getLogger(name);
-    }
-    return result;
+    return new Logger(name);
   }
 
+  private final Log innerLog;
+
   private Logger(String name) {
-    super(name, null);
+    this.innerLog = LogFactory.getLog(name);
   }
 
   public void message(String msg) {
-    log(Level.INFO, msg);
+    innerLog.info(msg);
   }
 
   public void error(Throwable e) {
-    this.log(Level.SEVERE, "Exception caught: ", e);
+    innerLog.error("Exception caught: ", e);
     throw new RuntimeException(e);
   }
 
   public void error(String message, Throwable e) {
-    this.log(Level.SEVERE, message, e);
+    innerLog.error(message, e);
     throw new RuntimeException(message, e);
   }
 
   public void assertTrue(boolean condition, String message) {
     if(!condition){
-      this.log(Level.WARNING, "Assertion failed: " + message);
+      innerLog.error("Assertion failed: " + message);
       throw new RuntimeException(message);
     }
   }
 
   public void warning(String message, Throwable th) {
-    this.log(Level.WARNING, message, th);
+    innerLog.warn(message, th);
+  }
+
+  public void warning(String message) {
+    innerLog.warn(message);
   }
 
   public void warning(Throwable th) {
-    this.log(Level.WARNING, "Exception occured!", th);
+    innerLog.warn("Exception occured!", th);
   }
 
   public void error(String message) {
-    this.log(Level.SEVERE, message);
+    innerLog.error(message);
     throw new RuntimeException(message);
   }
 
   public void info(CharSequence message) {
-    this.log(Level.INFO, message.toString());
+    innerLog.info(message.toString());
   }
+
+  // common logging deleging methods
+
+  public boolean isDebugEnabled() {
+    return innerLog.isDebugEnabled();
+  }
+
+  public boolean isErrorEnabled() {
+    return innerLog.isErrorEnabled();
+  }
+
+  public boolean isFatalEnabled() {
+    return innerLog.isFatalEnabled();
+  }
+
+  public boolean isInfoEnabled() {
+    return innerLog.isInfoEnabled();
+  }
+
+  public boolean isTraceEnabled() {
+    return innerLog.isTraceEnabled();
+  }
+
+  public boolean isWarnEnabled() {
+    return innerLog.isWarnEnabled();
+  }
+
+  public void trace(final Object o) {
+    innerLog.trace(o);
+  }
+
+  public void trace(final Object o, final Throwable throwable) {
+    innerLog.trace(o, throwable);
+  }
+
+  public void debug(final Object o) {
+    innerLog.debug(o);
+  }
+
+  public void debug(final Object o, final Throwable throwable) {
+    innerLog.debug(o, throwable);
+  }
+
+  public void info(final Object o) {
+    innerLog.info(o);
+  }
+
+  public void info(final Object o, final Throwable throwable) {
+    innerLog.info(o, throwable);
+  }
+
+  public void warn(final Object o) {
+    innerLog.warn(o);
+  }
+
+  public void warn(final Object o, final Throwable throwable) {
+    innerLog.warn(o, throwable);
+  }
+
+  public void error(final Object o) {
+    innerLog.error(o);
+  }
+
+  public void error(final Object o, final Throwable throwable) {
+    innerLog.error(o, throwable);
+  }
+
+  public void fatal(final Object o) {
+    innerLog.fatal(o);
+  }
+
+  public void fatal(final Object o, final Throwable throwable) {
+    innerLog.fatal(o, throwable);
+  }
+
 }
