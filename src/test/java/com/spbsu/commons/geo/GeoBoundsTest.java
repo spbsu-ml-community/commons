aac8027307d0ac188470db24594a071583f510e5
Sergey Lyadzhin <lyadzhin@yandex-team.ru>
2014-07-28 13:59:54 +0400

    move GeoPoint and GeoBounds to commons

---
+++ src/test/java/com/spbsu/commons/geo/GeoBoundsTest.java
@@ -0,0 +1,17 @@
+package com.spbsu.commons.geo;
+
+import junit.framework.TestCase;
+
+/**
+ * User: zador
+ * Date: 07.02.2014 7:38 PM
+ */
+
+public class GeoBoundsTest extends TestCase {
+  public void testCenter() {
+    final GeoBounds geoBounds = new GeoBounds(GeoPoint.valueOf(50, 5), GeoPoint.valueOf(10, 35));
+    assertEquals(GeoPoint.valueOf(30, 20), geoBounds.getCenter());
+    assertEquals(30.0, geoBounds.getWidth());
+    assertEquals(40.0, geoBounds.getHeight());
+  }
+}
