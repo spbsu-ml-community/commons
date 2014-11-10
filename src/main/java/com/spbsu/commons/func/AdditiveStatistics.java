19fa7a359209b424ddb0ebc98f79a1fc8384b1ef
noxoomo <noxoomo@yandex-team.ru>
2014-11-10 17:05:45 +0300

    GreedyRegion with weak conditions

--- src/main/java/com/spbsu/commons/func/AdditiveStatistics.java
+++ src/main/java/com/spbsu/commons/func/AdditiveStatistics.java
@@ -1,7 +1,5 @@
 package com.spbsu.commons.func;
 
-import com.spbsu.commons.math.vectors.Vec;
-
 /**
  * User: solar
  * Date: 12.11.13
@@ -10,22 +8,9 @@ import com.spbsu.commons.math.vectors.Vec;
 public interface AdditiveStatistics {
   AdditiveStatistics append(int index, int times);
 
-  AdditiveStatistics append(int index, double weight, int times);
-
-  AdditiveStatistics append(int index, double weight);
-
-
   AdditiveStatistics append(AdditiveStatistics other);
 
   AdditiveStatistics remove(int index, int times);
 
   AdditiveStatistics remove(AdditiveStatistics other);
-
-  AdditiveStatistics remove(int index, double weight, int times);
-
-  AdditiveStatistics remove(int index, double weight);
-
-  Vec getTargets();
 }
-
-
