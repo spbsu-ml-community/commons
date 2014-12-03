a5c7dc47a500df31c7313d198b7fa70fc2efa885
vkokarev <vkokarev@yandex-team.ru>
2014-12-03 19:20:50 +0300

    fix delimeters (cherry picked from commit fae4546)

--- src/main/java/com/spbsu/commons/math/io/Vec2CharSequenceConverter.java
+++ src/main/java/com/spbsu/commons/math/io/Vec2CharSequenceConverter.java
@@ -1,5 +1,10 @@
 package com.spbsu.commons.math.io;
 
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+
 import com.spbsu.commons.func.Converter;
 import com.spbsu.commons.math.vectors.Vec;
 import com.spbsu.commons.math.vectors.VecIterator;
@@ -25,7 +30,6 @@ public class Vec2CharSequenceConverter implements Converter<Vec, CharSequence> {
     }
     else {
       result = new ArrayVec(CharSeqTools.parseInt(parts[0]));
-      final CharSequence[] idx2val = new CharSequence[2];
       for (int i = 1; i < parts.length; i++) {
         result.set(i - 1, CharSeqTools.parseDouble(parts[i]));
       }
@@ -35,19 +39,25 @@ public class Vec2CharSequenceConverter implements Converter<Vec, CharSequence> {
 
   @Override
   public CharSequence convertTo(Vec v) {
+    final NumberFormat prettyPrint = NumberFormat.getInstance(Locale.US);
+    prettyPrint.setMaximumFractionDigits(5);
+    prettyPrint.setMinimumFractionDigits(0);
+    prettyPrint.setRoundingMode(RoundingMode.HALF_UP);
+    prettyPrint.setGroupingUsed(false);
+
     StringBuilder builder = new StringBuilder();
     builder.append(v.dim());
     if (v instanceof SparseVec) {
       final VecIterator it = v.nonZeroes();
       while(it.advance()) {
-        builder.append("\t");
-        builder.append(it.index()).append(":").append(it.value());
+        builder.append(" ");
+        builder.append(it.index()).append(":").append(prettyPrint.format(it.value()));
       }
     }
     else {
       for (int i = 0; i < v.dim(); i++) {
-        builder.append("\t");
-        builder.append(v.get(i));
+        builder.append(" ");
+        builder.append(prettyPrint.format(v.get(i)));
       }
     }
     return builder.toString();
