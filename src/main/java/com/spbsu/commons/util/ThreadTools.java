d079779dd4fb6fefb4e1e45ab7ef66cec35c5a7c
Igor Makunin <imakunin@yandex-team.ru>
2014-12-18 14:24:32 +0300

    turn off stdout & stderr redirect to logger set console log level to ERROR thread pools set uncaught exception handler

--- src/main/java/com/spbsu/commons/util/ThreadTools.java
+++ src/main/java/com/spbsu/commons/util/ThreadTools.java
@@ -4,28 +4,40 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.*;
 
+
+import org.apache.commons.logging.LogFactory;
+
 /**
  * User: solar
  * Date: 17.09.13
  * Time: 14:42
  */
-public class ThreadTools {
-  public static int COMPUTE_UNITS = Runtime.getRuntime().availableProcessors();
-  public static ThreadPoolExecutor createBGExecutor(final String name, int queueSize) {
+public final class ThreadTools {
+
+  private static final int COMPUTE_UNITS = Runtime.getRuntime().availableProcessors();
+
+  private ThreadTools() {
+  }
+
+  @NotNull
+  public static ThreadPoolExecutor createBGExecutor(@NotNull final String name, final int queueSize) {
     final BlockingQueue<Runnable> workQueue;
-    if (queueSize > 0) workQueue = new ArrayBlockingQueue<Runnable>(queueSize);
-    else workQueue = new LinkedBlockingQueue<Runnable>();
+    if (queueSize > 0) {
+      workQueue = new ArrayBlockingQueue<>(queueSize);
+    } else {
+      workQueue = new LinkedBlockingQueue<>();
+    }
 
     return new ThreadPoolExecutor(COMPUTE_UNITS, COMPUTE_UNITS, 5, TimeUnit.SECONDS, workQueue, new ThreadFactory() {
       @NotNull
       @Override
-      public Thread newThread(Runnable r) {
+      public Thread newThread(@NotNull final Runnable r) {
         final Thread thread = new Thread(r, name);
         thread.setDaemon(true);
         thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
           @Override
-          public void uncaughtException(Thread t, Throwable e) {
-            e.printStackTrace();
+          public void uncaughtException(@NotNull final Thread t, @NotNull final Throwable e) {
+            LogFactory.getLog(ThreadTools.class).error("Thread " + t + " dead", e);
           }
         });
         return thread;
