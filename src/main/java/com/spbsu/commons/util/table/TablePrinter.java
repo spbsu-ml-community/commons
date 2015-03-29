157cbf44eb47d13f8ed312922e230591ed84acdc
qdeee <qdeee@yandex-team.ru>
2015-03-29 19:32:49 +0300

    Extract nested class in TablePrinter

--- src/main/java/com/spbsu/commons/util/table/TablePrinter.java
+++ src/main/java/com/spbsu/commons/util/table/TablePrinter.java
@@ -1,4 +1,4 @@
-package com.spbsu.commons.util;
+package com.spbsu.commons.util.table;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -326,49 +326,6 @@ public class TablePrinter {
     }
 
   }
-
-  public static class TableBuilder {
-    private final List<String[]> rows;
-    private String[] header;
-
-    public TableBuilder(final String... header) {
-      this();
-      this.header = header;
-    }
-
-    public TableBuilder() {
-      rows = new ArrayList<>();
-    }
-
-    public TableBuilder setHeader(final String description, final int... values) {
-      header = new String[values.length + 1];
-      header[0] = description;
-      for (int i = 0; i < values.length; i++) {
-        header[1 + i] = String.valueOf(values[i]);
-      }
-      return this;
-    }
-
-    public TableBuilder addRow(final String description, final double... values) {
-      final String[] row = new String[values.length + 1];
-      row[0] = description;
-      for (int i = 0; i < values.length; i++) {
-        row[1 + i] = String.format("%.6f", values[i]);
-      }
-      rows.add(row);
-      return this;
-    }
-
-    public TableBuilder addRow(final String... row) {
-      rows.add(row);
-      return this;
-    }
-
-    public String build() {
-      return getTable(header, rows.toArray(new String[][]{new String[0]}), ALIGN_LEFT);
-    }
-  }
-
 
 
 //  Example:
