f7fedb0f4fc5aea33d601639aaaaaf67626f3f5e
towelenee <belous.mikhail@gmail.com>
2015-05-14 15:53:27 +0300

    Polynomial Tree speed up

--- src/main/java/com/spbsu/commons/math/vectors/VectorOfMultiplicationsFactory.java
+++ src/main/java/com/spbsu/commons/math/vectors/VectorOfMultiplicationsFactory.java
@@ -1,35 +1,30 @@
 package com.spbsu.commons.math.vectors;
 
 import com.spbsu.commons.math.MathTools;
-import com.spbsu.commons.math.vectors.impl.vectors.ArrayVec;
 
 /**
  * Created by towelenee on 5/14/15.
  * Vector of multiplication for regression purposes
  */
-public class VectorOfMultiplications extends ArrayVec {
-  public VectorOfMultiplications(double[] data, int dimensions)
-  {
-    super(getMultiplications(data, dimensions), 0, MathTools.combinationsWithRepetition(data.length, dimensions));
+public class VectorOfMultiplicationsFactory{
+  public int getDim() {
+    return dim;
   }
 
-  private static double[] getMultiplications(double[] data, int dimensions) {
+  private final int dim;
+  private final int[][] indexes;
+  public VectorOfMultiplicationsFactory(int maxIndex, int dimensions)
+  {
+    dim = MathTools.combinationsWithRepetition(maxIndex, dimensions);
+    indexes = new int[dim][];
     int[] index = new int[dimensions];
-    final int size = MathTools.combinationsWithRepetition(data.length, dimensions);
-    final double[] answer = new double[size];
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < dim; i++)
     {
-      {
-        double value = 1;
-        for (int j = 0; j < index.length; j++)
-          value *= data[index[j]];
-        answer[i] = value;
-      }
+      indexes[i] = index.clone();
       generateNextDecreasingIndex(index);
     }
-    if (hasNextDecreasingIndex(index, data.length))
+    if (hasNextDecreasingIndex(index, maxIndex))
       throw new RuntimeException("Bug in multiplication generation");
-    return answer;
   }
 
   private static boolean hasNextDecreasingIndex(final int[] index, final int maxValue) {
@@ -46,5 +41,12 @@ public class VectorOfMultiplications extends ArrayVec {
     index[j]++;
     for (int i = j + 1; i < index.length; i++)
       index[i] = 0;
+  }
+
+  public double get(Vec data, int index) {
+    double multiplication = 1;
+    for(int i = 0; i < indexes[index].length; i++)
+      multiplication *= data.get(indexes[index][i]);
+    return multiplication;
   }
 }
